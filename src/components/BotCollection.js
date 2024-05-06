b383c8583032f830b9dbe9765d586dea854cf5a9
isackmwangasu <mwangasuisack@gmail.com>
2024-05-06 23:12:01 +0300

    minor changes

--- src/components/BotCollection.js
+++ src/components/BotCollection.js
@@ -1,15 +1,20 @@
-import React from "react";
 
-function BotCollection() {
-  // Your code here
+import React from 'react'
+import BotCard from "./BotCard"
+
+ function BotCollection({ botCollection, action, removeCard }) {
+  const displayBotCards = botCollection.map(bot => {
+    return <BotCard bot={bot} action={action} removeCard={removeCard} />
+  })
+
   return (
     <div className="ui four column grid">
       <div className="row">
-        {/*...and here..*/}
+        {displayBotCards}
         Collection of all bots
       </div>
     </div>
-  );
-}
+  )
+ }
 
-export default BotCollection;
+ export default BotCollection
